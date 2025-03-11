3ce90d1892eb95480d0ada5cf4521cfba8342631
GoodnewsPerfect <samuelgoodnews15@gmail.com>
2025-03-11 22:53:05 +0100

    Fix: fixed search error

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\Blog;
+use App\Models\Creator; 
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -11,14 +12,19 @@ class SearchController extends Controller
     public function search(Request $request)
     {
         $query = $request->get('query');
-        
+
         $products = Product::with(['category', 'subCategory', 'variants.images'])
-            ->where('name', 'like', "%{$query}%")
-            ->orWhere('description', 'like', "%{$query}%")
-            ->orWhere('creator', 'like', "%{$query}%")
+            ->where(function ($q) use ($query) {
+                $q->where('name', 'like', "%{$query}%")
+                  ->orWhere('description', 'like', "%{$query}%");
+            })
             ->take(5)
             ->get();
-            
+
+        $creators = Creator::where('name', 'like', "%{$query}%")
+            ->take(5)
+            ->get();
+
         $blogs = Blog::with('media')
             ->where('title', 'like', "%{$query}%")
             ->orWhere('content', 'like', "%{$query}%")
@@ -29,6 +35,7 @@ class SearchController extends Controller
             'status' => 'success',
             'data' => [
                 'products' => $products,
+                'creators' => $creators, 
                 'blogs' => $blogs
             ]
         ]);
